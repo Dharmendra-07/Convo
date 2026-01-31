21dbde39d6d6ea55bd99ec601c08a6175e30a111
Dharmendra Kumar <justdk06@gmail.com>
2026-02-01 01:40:56 +0530

    feat: intergrate Clerk authentication and set up user routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,17 @@
 import express from 'express'
+import { clerkMiddleware } from '@clerk/express'
 
 import authRoutes from "./routes/authRoutes"
 import chatRoutes from "./routes/chatRoutes"
 import messageRoutes from "./routes/messageRoutes"
 import userRoutes from "./routes/userRoutes"
+import { errorHandler } from './middleware/errorHandler'
 
 const app = express();
 
 app.use(express.json()) // parses incoming JSON request bodies and makes them available as req. body in your route handlers.
 
+app.use(clerkMiddleware())
 app.get('/health', (req, res)=>{
     res.json({status: "ok", message: "Server is running"});
 });
@@ -17,5 +20,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/chats", chatRoutes);
 app.use("/api/messageRoutes", messageRoutes);
 app.use("/api/user", userRoutes);
+
+
+// error handlers must come after all the routes and other middlewares so they can catch error passed with next(err) or thrown inside async handlers.
+app.use(errorHandler);
 
 export default app;
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,5 +1,13 @@
 import { Router } from "express";
+import { getMe, authCallback } from '../controllers/authController';
+import { protectRoute  } from "../middleware/auth";
+
 
 const router = Router();
+
+// /api/auth/me
+
+router.get('/me',protectRoute, getMe);
+router.post('/callback', authCallback);
 
 export default router;
