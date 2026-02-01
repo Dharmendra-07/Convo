4cf461f9e8f549596e976ca2323a1979d6e71cbe
Dharmendra Kumar <justdk06@gmail.com>
2026-02-02 01:22:28 +0530

    feat: add Dockerfile and serve frontend in production

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import path from 'path';
+
 import { clerkMiddleware } from '@clerk/express'
 
 import authRoutes from "./routes/authRoutes"
@@ -24,5 +26,16 @@ app.use("/api/user", userRoutes);
 
 // error handlers must come after all the routes and other middlewares so they can catch error passed with next(err) or thrown inside async handlers.
 app.use(errorHandler);
+
+// serve frontend in production 
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "../../web/dist")))
+
+    app.get("/{*any}", (_, res) => {
+        res.sendFile(path.join(__dirname, "../../web/dist/index.html"))
+    })
+
+}
 
 export default app;
