6c0653e4b62842f0b1788afd488a1bd64a508acb
Dharmendra Kumar <justdk06@gmail.com>
2026-02-03 12:51:54 +0530

    Axios. - Updated .gitignore to exclude .env.local files. - Refactored index.tsx and _layouy.tsx clearner imports and structure

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -4,53 +4,53 @@ import type { NextFunction, Request, Response } from "express";
 import { clerkClient, getAuth } from "@clerk/express";
 
 // todo: add the next later
-export async function getMe(req: AuthRequest, res: Response, next: NextFunction){
+export async function getMe(req: AuthRequest, res: Response, next: NextFunction) {
     try {
         const userId = req.userId
 
         const user = await User.findById(userId)
 
-        if (!user){
-            res.status(404).json({ message: 'User not found'});
+        if (!user) {
+            res.status(404).json({ message: 'User not found' });
             return;
         }
 
         res.status(200).json(user)
     } catch (error) {
-       res.status(500);
-       next(error);
+        res.status(500);
+        next(error);
     }
 }
 
 export async function authCallback(req: Request, res: Response, next: NextFunction) {
     try {
-        const {userId:clerkId} = getAuth(req);
+        const { userId: clerkId } = getAuth(req);
 
-        if (!clerkId){
-            res.status(401).json({ message: 'Unauthorized'});
+        if (!clerkId) {
+            res.status(401).json({ message: 'Unauthorized' });
             return;
         }
 
-        let user = await User.findOne({clerkId})
+        let user = await User.findOne({ clerkId })
 
-        if(!user){
-           // get user into from clerk and save to db
-           const clerkUser = await clerkClient.users.getUser(clerkId)
+        if (!user) {
+            // get user into from clerk and save to db
+            const clerkUser = await clerkClient.users.getUser(clerkId)
 
-           user = await User.create({
-            clerkId,
-            name: clerkUser.firstName 
-                ? `${clerkUser.firstName} ${clerkUser.lastName || ""}`.trim() 
-                : clerkUser.emailAddresses[0]?.emailAddress?.split('@')[0],
-            email: clerkUser.emailAddresses[0]?.emailAddress,
-            avatar: clerkUser.imageUrl
-           });
+            user = await User.create({
+                clerkId,
+                name: clerkUser.firstName
+                    ? `${clerkUser.firstName} ${clerkUser.lastName || ""}`.trim()
+                    : clerkUser.emailAddresses[0]?.emailAddress?.split('@')[0],
+                email: clerkUser.emailAddresses[0]?.emailAddress,
+                avatar: clerkUser.imageUrl
+            });
         }
 
         res.json(user);
     } catch (error) {
         res.status(500)
         next(error)
-        
+
     }
 }
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,17 +1,44 @@
 import { Stack } from "expo-router";
 import '../global.css'
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import AuthSync from "@/components/AuthSync";
+import { StatusBar } from "expo-status-bar";
+import * as Sentry from '@sentry/react-native';
 
+Sentry.init({
+  dsn: 'https://1f2c5ccb471c8b438f202cae514abffa@o4510819689693184.ingest.de.sentry.io/4510819697688656',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 const queryClient = new QueryClient()
 
-
-export default function RootLayout() {
+export default Sentry.wrap(function RootLayout() {
   return (
-    <QueryClientProvider client={queryClient}>
-       <Stack screenOptions={{headerShown: false}}/>
-
-    </QueryClientProvider>
+    <ClerkProvider tokenCache={tokenCache}>
+      <QueryClientProvider client={queryClient}>
+        <AuthSync />
+        <StatusBar style="light"/>
+        <Stack screenOptions={{ headerShown: false, contentStyle: { 
+          backgroundColor: '#0D0D0F' }}} />
+        <Stack.Screen name='(auth)' options={{ animation: 'fade' }} />
+        <Stack.Screen name='(auth)' options={{ animation: 'fade' }} />
+      </QueryClientProvider>
+    </ClerkProvider>
   )
-
-}
+});
